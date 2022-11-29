c9029027bd6d95f7124c137ce260240a3e30ff85
Erikka Polk <erikka.pokl@gmail.com>
2022-11-28 18:59:15 -0600

    I put repeated code into a function, made each unique filename a variable, and precisely ordered the function calls

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,31 @@
+
+def org_file_content(filename):
+    """organize files...by opening them/creating an object, 
+    striping them of white space, and delimiting them by "|" 
+    and putting each line into list"""
+    the_file = open(filename)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+
+#script starts here:
 print("\nDay 1\n")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
+filename = "um-deliveries-day-1.txt"
+org_file_content(filename)
 
 print("\nDay 2\n")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
+filename = "um-deliveries-day-2.txt"
+org_file_content(filename)
 
 print("\nDay 3\n")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+filename = "um-deliveries-day-3.txt"
+org_file_content(filename)
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
