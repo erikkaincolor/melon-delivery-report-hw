3ef52796b9f76aa75b67f8ec87693239a25e42eb
Erikka Polk <erikka.pokl@gmail.com>
2022-11-28 19:04:27 -0600

    clarified process in comments

--- produce_summary.py
+++ produce_summary.py
@@ -3,17 +3,22 @@ def org_file_content(filename):
     """organize files...by opening them/creating an object, 
     striping them of white space, and delimiting them by "|" 
     and putting each line into list"""
-    the_file = open(filename)
-    for line in the_file:
-        line = line.rstrip()
-        words = line.split('|')
+    the_file = open(filename) # Create a file object from the string passed in as path
+    #^^^^is associated with line 17: "the_file.close()"
 
-        melon = words[0]
+    for line in the_file:
+        line = line.rstrip() # Remove extra whitespace to the right of the line
+        words = line.split('|') # Create a list of strings
+
+        melon = words[0] # Assign a meaningful variable name to each item from the words list
         count = words[1]
         amount = words[2]
 
         print(f"Delivered {count} {melon}s for total of ${amount}")
-    the_file.close()
+    the_file.close() #The close() method closes an open file.
+# You should always close your files in some cases, 
+# due to buffering, changes made to a file may not 
+# show until you close the file
 
 
 #script starts here:
